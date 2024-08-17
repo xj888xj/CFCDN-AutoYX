ddccd07f3e0825f77bee41e3d4fbbeba1210b924
renzisen <xj888xj@qq.com>
2024-08-18 06:20:22 +0800

    Update cloudflarest-cesu.sh

--- cloudflarest-cesu.sh
+++ cloudflarest-cesu.sh
@@ -14,37 +14,6 @@ SAVE_PATH="${FDIP_DIR}/txt.zip"
 mkdir -p "${FDIP_DIR}"
 mkdir -p "${CFST_DIR}"
 
-# 1. 下载 txt.zip 文件
-echo "============================开始下载txt.zip============================="
-download_url="https://zip.baipiao.eu.org/"
-wget "${download_url}" -O "${SAVE_PATH}"
-if [ $? -ne 0 ]; then
-    echo "下载失败，脚本终止。"
-    exit 1
-fi
-
-# 2. 解压 txt.zip 文件到 FDIP 文件夹
-echo "===============================解压txt.zip==============================="
-unzip -o "${SAVE_PATH}" -d "${FDIP_DIR}"
-
-# 3. 合并并去重指定的文件
-echo "==============================合并和去重文件============================="
-#cat "${FDIP_DIR}/45102-1-443.txt" "${FDIP_DIR}/31898-1-443.txt" | sort -u > "${FDIP_DIR}/all.txt"
-awk '!seen[$0]++' "${FDIP_DIR}/45102-1-443.txt" "${FDIP_DIR}/31898-1-443.txt" > "${FDIP_DIR}/all.txt"
-
-# 4. 读取 all.txt 并查询归属地，保留 SG（新加坡）的IP地址到 sg.txt
-echo "=========================筛选国家代码为SG的IP地址=========================="
-while IFS= read -r ip; do
-    country_code=$(curl -s "https://ipapi.co/$ip/country/" | tr -d '[:space:]')
-    if [ "$country_code" == "SG" ]; then
-        echo $ip >> "${CFST_DIR}/sg.txt"
-    fi
-done < "${FDIP_DIR}/all.txt"
-
-# 5. 删除 FDIP 文件夹中除了 all.txt 文件之外的所有文件
-echo "============================清理不必要的文件============================="
-find "${FDIP_DIR}" -type f ! -name 'all.txt' -delete
-
 # 6. 下载 CloudflareST_linux_amd64.tar.gz 文件到 CloudflareST 文件夹
 echo "=========================下载和解压CloudflareST=========================="
 if [ ! -f "${CFST_DIR}/CloudflareST" ]; then
